513e1f723a1c8d094fc2e37675f3a430aebe59f0
JaimeGDH <JaimeGDH@users.noreply.github.com>
2026-08-18 17:04:08 -0400

    Fix local.properties missing error in CI settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -2,9 +2,14 @@ pluginManagement {
     val flutterSdkPath =
         run {
             val properties = java.util.Properties()
-            file("local.properties").inputStream().use { properties.load(it) }
+            val localPropertiesFile = settingsDir.resolve("local.properties")
+            if (localPropertiesFile.exists()) {
+                localPropertiesFile.inputStream().use { properties.load(it) }
+            }
             val flutterSdkPath = properties.getProperty("flutter.sdk")
-            require(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
+                ?: System.getenv("FLUTTER_ROOT")
+                ?: System.getenv("FLUTTER_HOME")
+                ?: throw java.lang.IllegalStateException("flutter.sdk not set in local.properties and FLUTTER_ROOT is not set")
             flutterSdkPath
         }
 
